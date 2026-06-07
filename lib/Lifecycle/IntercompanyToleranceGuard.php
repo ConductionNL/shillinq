56769616df566296dc5dc6c75bee9da45e585497
Hydra Pipeline <hydra-pipeline@conduction.nl>
2026-06-07 06:31:53 +0000

    fix(code-review bounded): Juan post-run mechanical commit

--- lib/Lifecycle/IntercompanyToleranceGuard.php
+++ lib/Lifecycle/IntercompanyToleranceGuard.php
@@ -29,6 +29,9 @@
  * @link https://conduction.nl
  *
  * @spec openspec/changes/bookkeeping-intercompany-elimination/tasks.md#task-13
+ *
+ * SPDX-FileCopyrightText: 2026 Conduction B.V. <info@conduction.nl>
+ * SPDX-License-Identifier: EUPL-1.2
  */
 
 declare(strict_types=1);
